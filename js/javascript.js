49d1f5e8ab6f907efe9b1d6c8dd4f8109627f090
Armando Interaminense <99498988+ainteraminense@users.noreply.github.com>
2026-03-21 15:19:22 -0400

    Make grid dynamic based on customizable size and initiate with size equal to 16

--- js/javascript.js
+++ js/javascript.js
@@ -1,18 +1,23 @@
 // create variables
 const container = document.querySelector(".container");
-// const spawns = [];
+const gridMaker = document.querySelector("button");
 let spawn;
-// let spawnsRow;
+
 // create 16 x 16 grid
 // using flex
 // pay attention borders and margins
-function createGrid() {
-    for (let i = 0; i < 16; i++) {
+function createGrid(size) {
+    const width = 960/size;
+    const height = 960/size; 
+    for (let i = 0; i < size; i++) {
         let spawnsRow = document.createElement("div");
         spawnsRow.classList.add("spawns-row");
-        for (let j = 0; j < 16; j++) {
+        spawnsRow.style.height = height + "px";
+        for (let j = 0; j < size; j++) {
             spawn = document.createElement("div");
             spawn.classList.add("spawns");
+            spawn.style.width = width + "px";
+            spawn.style.height = height + "px";
             spawn.addEventListener("mouseover", (event) => {
                 event.target.style.backgroundColor = "gray";
             });
@@ -22,5 +27,4 @@ function createGrid() {
     }
 }
 
-
-createGrid();
+createGrid(16);
